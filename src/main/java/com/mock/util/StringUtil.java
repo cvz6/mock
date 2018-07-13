d2328a6492702f2281ee959b82b754ed0b93c745
lp <1029538990@qq.com>
2018-07-13 12:08:49 +0800

    增加诗词作为string的种子

---
+++ src/main/java/com/mock/util/StringUtil.java
@@ -0,0 +1,63 @@
+package com.mock.util;
+
+import java.io.*;
+
+/**
+ * @author :  peng.liu
+ * @description : 字符串工具
+ * @date :    2018/7/13 11:09
+ */
+public class StringUtil {
+
+
+    /**
+     * 去除string的所有空白字符 包括回车，空格，制表符等
+     *
+     * @param str 字符串
+     * @return String
+     */
+    public static String trimAll(String str) {
+        return str.replaceAll("\\s*", "");
+    }
+
+    /**
+     * 去除指定文件 文件内容的所有空白字符 包括回车，空格，制表符等
+     *
+     * @param path 文件路径
+     * @return String
+     */
+    public static String trimFile(String path) {
+        StringBuilder text = new StringBuilder();
+        //读取文件
+        File file = new File(path);
+        if (file.isFile() && file.exists()) { //判断文件是否存在
+            String line = null;
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))
+            ) {
+                while ((line = bufferedReader.readLine()) != null) {
+                    text.append(trimAll(line.toString()));  //去除空白字符
+                }
+                System.out.println(text);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //写回文件
+            try (
+                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)))
+            ) {
+                bufferedWriter.write(text.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            System.err.println("找不到指定的文件");
+        }
+        return text.toString();
+    }
+
+    public static void main(String[] args) {
+        trimFile("C:\\workspace\\mock\\src\\main\\resources\\String.text");
+    }
+}
